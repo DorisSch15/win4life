56a5e13899f75163751007ce32bdc89ec440dcbf
Doris <sayitloud15@gmail.com>
2022-10-26 18:04:25 +0200

    index.js - added front- and back-side for game-section__item and win-section__number to flip the divs with a click event

--- src/js/index.js
+++ src/js/index.js
@@ -3,43 +3,55 @@ import './../assets/styles/main.scss';
 import { gambleNumbers, winNumbers, letsGamble } from './game.js';
 
 let winSection = document.querySelector('.win-section__table');
-
 let gameSection = document.querySelector('.game-section');
 
-let gameBtn = document.querySelector('.getNewCard');
-gameBtn.addEventListener('click', render);
+winSection.addEventListener('click', changeZindex);
+gameSection.addEventListener('click', changeZindex);
+
+render()
+
 
 function render() {
     letsGamble();
-
+    
     winSection.innerHTML = '';
     for(let number of winNumbers){
         let winBox = document.createElement('div');
-
+        
         winBox.classList.add('win-section__number');
-
+        
         winBox.innerHTML = `
-            <div class="win-section__number-int">${number.int}</div>
-            <div class="win-section__number-string">${number.string}</div>
+        <div class="win-section__number-front">test</div>
+        <div class="win-section__number-back">
+            <div class="win-section__number-back-int">${number.int}</div>
+            <div class="win-section__number-back-string">${number.string}</div>
+        </div>
         `;
-
         winSection.appendChild(winBox);
     };
-
+    
     gameSection.innerHTML = '';
     for(let number of gambleNumbers){
         let gameBox = document.createElement('div');
         gameBox.classList.add('game-section__item');
-
+        
         gameBox.innerHTML = `
-        <div class="game-section__item__number-int">${number.int}</div>
-        <div class="game-section__item__number-string">${number.string}</div>
-        <div class="game-section__item__win-int">${number.win}${number.currency}</div>
-        <div class="game-section__item__win-string">${number.text}</div>
+        <div class="game-section__item-front">test</div>
+        <div class="game-section__item-back">
+            <div class="game-section__item-back__number-int">${number.int}</div>
+            <div class="game-section__item-back__number-string">${number.string}</div>
+            <div class="game-section__item-back__win-int">${number.win}${number.currency}</div>
+            <div class="game-section__item-back__win-string">${number.text}</div>
+        </div>
         `;
-
+        
         gameSection.appendChild(gameBox);
     };
 };
 
-render()
+
+function changeZindex(e){
+    if(e.target){
+        e.target.style.zIndex = 0;
+    };
+};
