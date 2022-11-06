8a9363ff8c9f01c0594e5bc91d3f6a2c72f8b679
Doris <sayitloud15@gmail.com>
2022-11-06 18:45:39 +0100

    wins.js - chanced win into type integer

--- src/js/wins.js
+++ src/js/wins.js
@@ -1,42 +1,42 @@
 export let wins = [
     {
-        win: "5",
+        win: 5,
         currency: "Fr",
         text: "fünf"
     }, {
-        win: "10",
+        win: 10,
         currency: "Fr",
         text: "zehn"
     }, {
-        win: "15",
+        win: 15,
         currency: "Fr",
         text: "fünfzehn"
     }, {
-        win: "20",
+        win: 20,
         currency: "Fr",
         text: "zwanzig"
     }, {
-        win: "50",
+        win: 50,
         currency: "Fr",
         text: "fünfzig"
     }, {
-        win: "500",
+        win: 500,
         currency: "Fr",
         text: "fünfhundert"
     }, {
-        win: "1'000",
+        win: 1000,
         currency: "Fr",
         text: "tausend"
     }, {
-        win: "5'000",
+        win: 5000,
         currency: "Fr",
         text: "fünftausend"
     }, {
-        win: "10'000",
+        win: 10000,
         currency: "Fr",
         text: "zehntausend"
     }, {
-        win: "20Jahre",
+        win: "20 Jahre",
         currency: "",
         text: "zwanzig"
     }
