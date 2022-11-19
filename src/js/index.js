848b0382229984534e29f2af41a5072a95c97c55
Doris <sayitloud15@gmail.com>
2022-11-19 22:25:45 +0100

    index.js - added scratched class so that the scratched items stay in front. f.e. when you load the page

--- src/js/index.js
+++ src/js/index.js
@@ -1,30 +1,39 @@
 import './../assets/styles/main.scss';
 
-import { gambleNumbers, winNumbers, letsGamble } from './game.js';
+import { gambleNumbers, winNumbers, createNewCard, CheckExistingCard } from './game.js';
 import {checkWin, setScratched} from './check-win.js';
 import {checkUserData} from './user.js';
 
-let winSection = document.querySelector('.win-section__table');
-let gameSection = document.querySelector('.game-section__table');
+export let winSection = document.querySelector('.win-section__table');
+export let gameSection = document.querySelector('.game-section__table');
+
+window.addEventListener('load', checkUserData);
 
 winSection.addEventListener('click', scratchItem);
 gameSection.addEventListener('click', scratchItem);
 
-window.addEventListener('load', checkUserData);
 
-export function render() {
-    letsGamble();
+export function renderCard() {
+    CheckExistingCard();
+
     let index = 0;
     
     winSection.innerHTML = '';
     for(let number of winNumbers){
+
+        let scratchedClass = '';
+
+        if (number.scratched === true){
+            scratchedClass = 'win-section__number-front--scratched';
+        };
+
         let winBox = document.createElement('div');
         
         winBox.classList.add('win-section__number');
         
         winBox.innerHTML = `
-        <div class="win-section__number-front fa-solid fa-clover" id="${index}-win"></div>
-        <div class="win-section__number-back">
+        <div class="win-section__number-front ${scratchedClass} fa-solid fa-clover" id="${index}-win"></div>
+        <div class="win-section__number-back win-section__number-back">
         <div class="win-section__number-back-int">${number.int}</div>
         <div class="win-section__number-back-string">${number.string}</div>
         </div>
@@ -38,36 +47,42 @@ export function render() {
     
     gameSection.innerHTML = '';
     for(let number of gambleNumbers){
+
+        let scratchedClass = '';
+        
+        if (number.scratched === true){
+            scratchedClass = 'game-section__item-front--scratched';
+        };
+
         let gameBox = document.createElement('div');
 
         gameBox.classList.add('game-section__item');
-        
         gameBox.innerHTML = `
-        <div class="game-section__item-front fa-solid fa-money-bill-1-wave" id="${index}-game"></div>
+        <div class="game-section__item-front ${scratchedClass} fa-solid fa-money-bill-1-wave" id="${index}-game"></div>
         <div class="game-section__item-back">
-            <div class="game-section__item-back__number-int">${number.int}</div>
-            <div class="game-section__item-back__number-string">${number.string}</div>
-            <div class="game-section__item-back__win-int">${checkString(number)}</div>
-            <div class="game-section__item-back__win-string">${number.text}</div>
+        <div class="game-section__item-back__number-int">${number.int}</div>
+        <div class="game-section__item-back__number-string">${number.string}</div>
+        <div class="game-section__item-back__win-int">${checkString(number)}</div>
+        <div class="game-section__item-back__win-string">${number.text}</div>
         </div>
         `;
         
         index += 1;
         gameSection.appendChild(gameBox);
+
     };
 };
 
-
 function scratchItem(e){
     if(e.target && (e.target.classList.contains('game-section__item-front') || e.target.classList.contains('win-section__number-front'))){
-        e.target.style.zIndex = 0;
         setScratched(e.target.id);
+        renderCard();
         checkWin();
     };
 };
 
 function checkString(number){
-    if(typeof number.win === "string"){
+    if(typeof number.win === 'string'){
         return number.win;
     } else {
         return getCurrencyFormat(number.win) + ' ' + number.currency;
